a97429c716e5f32d93110333d1c8457125073bd6
Harshavardhan Unnibhavi <harshanavkis@gmail.com>
2022-03-08 15:04:23 +0000

    gem5: nix shell for build requirements
    Signed-off-by: Harshavardhan Unnibhavi <harshanavkis@gmail.com>

---
+++ default.nix
@@ -0,0 +1,46 @@
+with import <nixpkgs> {};
+
+let
+  buildInputs = [
+    python3
+    protobuf
+    zlib
+    boost
+    gperftools
+    ncurses
+    libpng
+    hdf5
+  ];
+
+  ld = writeShellScriptBin "ld" ''
+     exec ${gcc7Stdenv.cc}/bin/ld ${lib.concatMapStringsSep " " (l: "-L${lib.getLib l}/lib -rpath ${lib.getLib l}/lib" ) buildInputs} "$@"
+  '';
+  args = lib.concatMapStringsSep " " (l: "-I${lib.getDev l}/include -L${lib.getLib l}/lib -Wl,-rpath,${lib.getLib l}/lib" ) buildInputs;
+  cc = writeShellScriptBin "cc" ''
+    exec ${gcc7Stdenv.cc}/bin/cc ${args} "$@"
+  '';
+  cxx = writeShellScriptBin "c++" ''
+    exec ${gcc7Stdenv.cc}/bin/c++ ${args} "$@"
+  '';
+in gcc7Stdenv.mkDerivation rec {
+  name = "env";
+  EDITOR = "vim";
+  M4 = "m4";
+  inherit buildInputs;
+
+  shellHook = ''
+    export CC=cc CXX=c++ LD=ld
+    export PATH=${ld}/bin:${cxx}/bin:${cc}/bin:$PATH
+  '';
+
+  nativeBuildInputs = [
+    python3.pkgs.pyyaml
+    git
+    gdb
+    scons
+    swig
+    m4
+    pkg-config
+    tree
+  ];
+}
